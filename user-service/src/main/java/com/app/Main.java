9b7d36947ea216f02b1400562ceabddb028cc4f0
Victor Lytsus <vlytsus@gmail.com>
2019-08-06 23:36:25 +0200

    Added @EnableEurekaClient to user-service

--- user-service/src/main/java/com/app/Main.java
+++ user-service/src/main/java/com/app/Main.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
 public class Main {
     public static void main(String[] args) {
