b0e8dfa68e5a73f9607bd32875be13b5e860e896
Viktor Varenikov <Viktor_Varenikov@epam.com>
2019-08-08 14:41:36 +0200

    Aadded email configurations

--- user-service/src/main/java/com/app/Configurations.java
+++ user-service/src/main/java/com/app/Configurations.java
@@ -1,9 +1,73 @@
 package com.app;
 
+import com.app.email.EmailService;
+import com.app.email.EmailServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
+@Slf4j
 @Configuration
+@PropertySource("classpath:application.properties")
+//@ConfigurationProperties(prefix="spring.mail")
 public class Configurations {
+
+    @Autowired
+    @Value("${spring.mail.username}")
+    private String username;
+
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+
+    @Autowired
+    @Value("${spring.mail.password}")
+    private String password;
+
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+
+    @Autowired
+    @Value("${spring.mail.host}")
+    private String host;
+
+//    public void setHost(String host) {
+//        this.host = host;
+//    }
+
+
+    @Autowired
+    @Value("${spring.mail.port}")
+    private int port;
+//    public void setPort(int port) {
+//        this.port = port;
+//    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 
 }
--- user-service/src/main/java/com/app/email/EmailServiceImpl.java
+++ user-service/src/main/java/com/app/email/EmailServiceImpl.java
@@ -1,6 +1,9 @@
 package com.app.email;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -8,8 +11,18 @@ import java.util.UUID;
 @Service
 @Slf4j
 public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
     @Override
     public void verifyEmail(UUID userId, String email){
         log.debug("verifyEmail: ", email);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("scg.victa@gmail.com");
+        message.setSubject("test message from micro service");
+        message.setText("Example message text");
+   //     mailSender.send(message);
     }
 }
