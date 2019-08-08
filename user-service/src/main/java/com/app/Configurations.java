7f80912e3bfbbbffab1e96ddbaad9ae0cf10f291
Viktor Varenikov <Viktor_Varenikov@epam.com>
2019-08-08 14:44:14 +0200

    Changes in using emeil configuration

--- user-service/src/main/java/com/app/Configurations.java
+++ user-service/src/main/java/com/app/Configurations.java
@@ -1,11 +1,8 @@
 package com.app;
 
-import com.app.email.EmailService;
-import com.app.email.EmailServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -17,40 +14,23 @@ import java.util.Properties;
 @Slf4j
 @Configuration
 @PropertySource("classpath:application.properties")
-//@ConfigurationProperties(prefix="spring.mail")
 public class Configurations {
 
     @Autowired
     @Value("${spring.mail.username}")
     private String username;
 
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-
     @Autowired
     @Value("${spring.mail.password}")
     private String password;
-
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
 
     @Autowired
     @Value("${spring.mail.host}")
     private String host;
 
-//    public void setHost(String host) {
-//        this.host = host;
-//    }
-
-
     @Autowired
     @Value("${spring.mail.port}")
     private int port;
-//    public void setPort(int port) {
-//        this.port = port;
-//    }
 
     @Bean
     public JavaMailSender getJavaMailSender() {
