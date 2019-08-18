fc6e137983c3ae6fbae1f5cbe5fe08f04747a878
Victor Lytsus <vlytsus@gmail.com>
2019-08-19 00:12:01 +0200

    Fixed configs

--- api-gateway/src/main/java/com/app/Main.java
+++ api-gateway/src/main/java/com/app/Main.java
@@ -2,8 +2,12 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- configserver/README.md
+++ configserver/README.md
@@ -12,6 +12,6 @@ mvn clean spring-boot:run -Dspring.profiles.active=native
 
 ## To test in browser open: 
 http://host:port/contextPath/config-file-name/spring-profile
-http://localhost:7777/application/dev
+http://localhost:1111/application/dev
 
 
